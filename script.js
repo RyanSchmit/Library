044876b02c0b0b372f79a3c0c7ed2be0bf7cb86f
Ryan Sch <ryan@Ryans-Air.home>
2022-09-02 14:09:02 -0400

    Fixed Duplication problem

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let myLibrary = [new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet"), new Book("You", "Joe Will", "344 pages", "not read yet")];
+const submitBtn = document.querySelector(".submit-btn");
+let booksSection = document.querySelector(".books");
+
+let myLibrary = [new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet")];
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -7,12 +10,41 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
+function resetForm() {
+	let read = document.getElementById("read");
+	let inputs = document.querySelectorAll('form input');
+	inputs.forEach((input) => {
+		input.value = '';
+	})
+	read.checked = false;
+}
+
 function addBookToLibrary() {
+	let title = document.getElementById("title");
+	let author = document.getElementById("author");
+	let pages = document.getElementById("pages");
+	let read = document.getElementById("read");
+	myLibrary.push(new Book(title.value, author.value, pages.value, read.value));
+	resetForm();
+	displayBooks();
+}
+
+
+function displayBooks() {
+	booksSection .innerHTML = '';
 	myLibrary.forEach((book) => {
-		console.log(book);
+		let bookHtml = `<div class="book">
+		<p>${book.title}</p>
+		<p>${book.author}</p>
+		<p>${book.pages} pages </p>
+		<button>Read</button>
+		</div>`;
+		booksSection.innerHTML += bookHtml;
 	})
 }
 
-addBookToLibrary();
+displayBooks();
+
+submitBtn.addEventListener('click', addBookToLibrary);
 
 
